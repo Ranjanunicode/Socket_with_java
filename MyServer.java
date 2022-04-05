c9c92068f0ea05162e5e08bec6d07f7e4fdf3912
Ranjan Pandit <59310086+Ranjanunicode@users.noreply.github.com>
2022-04-05 23:43:57 +0530

    Update MyServer.java

--- MyServer.java
+++ MyServer.java
@@ -1,4 +1,4 @@
-  /* Server side code . Establishing server for clients for server-client text/message transfer */
+  /* Server side code . Establishing server for clients for server-client text/message transfer/communication */
 
   /* Importing necessary libraries */
     import java.net.*;  
